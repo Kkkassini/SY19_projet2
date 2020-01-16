e7f8bd6094cf573b3e7fa1356f964b5a3f1e0809
Aghiles <aghiles-hamache@hotmail.fr>
2020-01-16 22:29:20 +0100

    ajout rapport

--- regression/mais.R
+++ regression/mais.R
@@ -1,4 +1,4 @@
-setwd("D:/UTC/GI05/SY19/sy19_projet2")
+setwd("D:/UTC/GI05/SY19/sy19_projet2/regression")
 library(randomForest)
 library(splines)
 library(rpart)
@@ -6,13 +6,20 @@ library(MASS)
 library(gam)
 library(glmnet)
 library(e1071)
+library(corrplot)
+library(psych)
 
 data <- read.csv(file = "./mais_train.csv", row.names = 1)
 lapply(data,class)
 colnames(data)[2] <- "y"
+
+corrmat<- corr.test(data)
+corrplot(corrmat$r)
+
 data[, 1] <- as.factor(data[, 1]) # year_harvest
 data[, 3] <- as.factor(data[, 3]) # NUMD
 data[, 4] <- as.factor(data[, 4]) # IRR
+
 scaledData <- data
 scaledData[,-c(1,2,3,4)] <- scale(data[,-c(1,2,3,4)])
 p <- ncol(data) - 1
@@ -66,6 +73,12 @@ mse.glm <- rep(0,Kfolds)
 mse.glmpca <- rep(0,Kfolds)
 mse.lasso <- rep(0,Kfolds)
 mse.radial.svr <- rep(0,Kfolds)
+
+#too long to compute several times : stored here MSEs of GAM+SS with different numbers of principal components
+#optimalSmooth = matrix(0,2,19)
+#optimalSmooth[1,]=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
+#optimalSmooth[2,]=c(0.9735438,0.9333093,0.8714166,0.8640651,0.854973,0.8370029,0.8101192,0.7790544,0.7463693,0.7414166,0.73439577,0.7345332,0.735001109,0.7368100,0.7350548,0.7329999,0.7311002,0.7225448,0.7050164)
+
 
 for(k in 1 : Kfolds) {
   #non-parametric models : no intern cross-val
@@ -132,7 +145,7 @@ for(k in 1 : Kfolds) {
   
   #VII.SVR
   #radial basis
-  #optimal parameters : epsilon = 0.2, cost=2
+  #optimal parameters given by tune() : epsilon = 0.2, cost=2.
   #optimal.radial.svr = tune(svm, y~., data=scaledDataDummyIrrWithoutYearAndDep[folds!=k,],
                             #ranges=list(epsilon=seq(0,2,0.2), cost=1:10))
   fit.radial.svr <- svm(y~., data=scaledDataDummyIrrWithoutYearAndDep[folds!=k,],
@@ -143,29 +156,26 @@ for(k in 1 : Kfolds) {
 
 
 #au lieu du boxplot : plot les IC
-boxplot(mse.regLin,mse.tree,mse.randomForest,mse.bagging,mse.glm,mse.glmpca,mse.lasso,mse.radial.svr,optimalSmooth[2,])
+boxplot(mse.regLin,mse.tree,mse.randomForest,mse.bagging,mse.glm,mse.lasso,mse.radial.svr,optimalSmooth[2,],names=c("regLin","decisionTree","rForest","Bagging","GLM","Lasso reg","SVR","GAM+SS"))
 
-
-#we choose SVR
+# --> best model : SVR
 
 #need to tune hyperparameters for SVR.
-optimal.radial.svr = tune(svm, y~., data=scaledDataDummyIrrWithoutYearAndDep,
-                          ranges=list(epsilon=seq(0,2,0.2), cost=1:10))
+#optimal.radial.svr = tune(svm, y~., data=scaledDataDummyIrrWithoutYearAndDep,
+                          #ranges=list(epsilon=seq(0,2,0.2), cost=1:10))
+
+#estime MSE on random data :
+
+folds <- sample(1:Kfolds, n, replace=TRUE)
+estimate.MSE <- rep(0,Kfolds)
+for(k in 1:Kfolds) {
+  fit.radial.svr <- svm(y~., data=scaledDataDummyIrrWithoutYearAndDep[folds!=k,],epsilon=0.2,cost=2)
+  pred.radial.svr = predict(fit.radial.svr, newdata=scaledDataDummyIrrWithoutYearAndDep[folds==k,-c(1)])
+  estimate.MSE[k] <- mean((pred.radial.svr - scaledDataDummyIrrWithoutYearAndDep$y[folds==k])^2)
+}
+
+print(mean(estimate.MSE))
 
 
-#pas pu tester tous lesmodèles possibles : pas testé ridge, pas testé elasticnet, natural et smooth splines,
-
-#Pas de splines multidimensionnels : dimension de l'espace des prédicteurs tro pélevée et
-#taille du dataset trop petite
 
 
-
-optimalSmooth = matrix(0,2,19)
-optimalSmooth[1,]=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
-optimalSmooth[2,]=c(0.9735438,0.9333093,0.8714166,0.8640651,0.854973,0.8370029,0.8101192,0.7790544,0.7463693,0.7414166,0.73439577,0.7345332,0.735001109,0.7368100,0.7350548,0.7329999,0.7311002,0.7225448,0.7050164)
-
-
-
-
-
-#essayer la regression lineaire mixed : lme4. Supposition : les variables qualitatives peuvent se regrouper dans des classes inconnues
